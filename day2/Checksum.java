ca135579974ba26d3f2be33982a983083015014e
Aaron Goldbin <31373437+agoldbin@users.noreply.github.com>
2017-12-23 19:24:29 -0600

    refined part 1

--- day2/Checksum.java
+++ day2/Checksum.java
@@ -1,32 +1,68 @@
+// package coa2017.day2;
+
 import java.util.*;
 import java.io.*;
 
 public class Checksum {
-    ArrayList<Integer> lineList = new ArrayList<Integer>();
-    ArrayList<Integer> diffList = new ArrayList<Integer>();
+    ArrayList<Integer> intList = new ArrayList<Integer>();
+    public void runCheckSum() {
+        String inputLine;
+        String[] tokenArray;
+        int min;
+        int max;
+        int diff = 0;
+        int totalDiff = 0;
 
-    public String runCheckSum() {
         // read in file
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        try (Scanner s = new Scanner(new FileReader("input.txt"))) {
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                Scanner ls = new Scanner(line);
+                while(ls.hasNextInt()) {
+                    intList.add(ls.nextInt());
+                }
+                max = Collections.max(intList);
+                min = Collections.min(intList);
+                diff += (max - min);
+                System.out.println(diff);
+                intList.clear();
+
+            }
+
+/*        try (BufferedReader reader
+                = new BufferedReader(new FileReader("input.txt"))) {
 
             //read line of file
-            while(reader.readLine() != null) {
-            //while line > insert number into arraylist
+            while(reader.ready()) {
+                inputLine = reader.readLine();
+                tokenArray = inputLine.split("\\W");
+                for (int i = 0; i < tokenArray.length; i++) {
+                    if (tokenArray[i].isEmpty()) {
+                    } else {
+                        // lineList.add(((tokenArray[i])));
+                        if (Integer.parseInt(tokenArray[i]) > max) {
+                            max = Integer.parseInt(tokenArray[i]);
+                        }
+                        if (Integer.parseInt(tokenArray[i]) < min) {
+                            min = Integer.parseInt(tokenArray[i]);
+                        }
+                    }
 
-            //check arraylist for max and main
-
-            // store diff in arraylist
-
-            // clear arraylist
-
-
-
-            // add total of diffArrayList
+                    // System.out.println(tokenArray[i]);
+                }
+                diff = max - min;
+                max = 0;
+                min = 10000;
+                totalDiff += diff;
+                System.out.println(totalDiff);
             }
+*/
         } catch (FileNotFoundException fnfe) {
-            System.out.println("Error, input file not found " +
-                    fnfe.printStackTrace());
+            System.out.println("Error: input file not found");
+            fnfe.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("Whoops, something went wrong");
+            exception.printStackTrace();
         }
     }
 }
--- day2/ChecksumDriver.java
+++ day2/ChecksumDriver.java
@@ -1,3 +1,5 @@
+// package coa2017.day2;
+
 public class ChecksumDriver {
     public static void main(String[] args) {
         Checksum checksum = new Checksum();
